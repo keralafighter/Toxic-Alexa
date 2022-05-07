901ff80c3c9e7e390523e21593b8b4943b3f9e05
Drozoxo <105075735+keralafighter@users.noreply.github.com>
2022-05-07 22:28:19 +0530

    DS qr
    ★⟵𝄞⃝👾ͥ͢ ͭ ᷤ  ͫ͢ ͤᴅʀᴏᴢᴏxᴏ͢₂₀₀₄✿⃝⸙⃝

--- qr.js
+++ qr.js
@@ -8,7 +8,7 @@ WhatsAsena - Yusuf Usta
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./Turbo/');
+const {StringSession} = require('./Sappu/');
 const fs = require('fs');
 
 async function whatsAsena () {
